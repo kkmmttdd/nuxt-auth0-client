3615a4b1b95e6a42aa5ce9b9e083d86f154b4493
kkmmttdd <kkmmttdd@gmail.com>
2020-08-27 01:42:44 +0900

    Feat: Send request to backend

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,25 @@
     <div>
       you are loginned as {{this.$auth.$state}}
     </div>
+    <div>
+      <v-btn>{{ this.res }}</v-btn>
+    </div>
   </div>
 </template>
 <script>
+  import axios from 'axios'
 
-export default {
-  components: {
-  }
+  export default {
+    data: {
+      res: ''
+    },
+    components: {
+    },
+    mounted() {
+      this.res = axios.get("localhost:8080", {"headers": {
+          "authorization": localStorage.getItem("auth0_token.auth0")
+        }
+      })
+    }
 }
 </script>
